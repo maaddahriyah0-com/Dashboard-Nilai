17a29d05caeedaa1302f6637b0d3f5350eff062f
maaddahriyah0-com <maaddahriyah0@gmail.com>
2026-06-02 14:40:53 +0700

    Update exportHelpers.ts

--- src/utils/exportHelpers.ts
+++ src/utils/exportHelpers.ts
@@ -155,7 +155,8 @@ currentY += 5;
   
   doc.setFont('times', 'normal');
   doc.setFontSize(10);
-doc.text(`NSM: ${schoolInfo.nsm}   |   NPSN: ${schoolInfo.npsn}`, 105, currentY, { align: 'center'
+  
+  doc.text(`NSM: ${schoolInfo.nsm}   |   NPSN: ${schoolInfo.npsn}`, 105, currentY, { align: 'center'});
   currentY += 5;
   
   doc.text(`Alamat: ${schoolInfo.address}, Kec. ${schoolInfo.subdistrict}, ${schoolInfo.city}, Prov. ${schoolInfo.province}`, 105, currentY, { align: 'center' });
